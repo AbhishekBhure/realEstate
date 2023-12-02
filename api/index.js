216bbea3da812a43e6c55f2990d6d3b7caa90e07
kiran <kiransagarwal126@gmail.com>
2023-12-02 23:51:51 +0530

    created middleware function to handle errors

--- api/index.js
+++ api/index.js
@@ -1,28 +1,40 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoute.js";
-import authRouter from "./routes/authRoute.js"
+import authRouter from "./routes/authRoute.js";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-dotenv.config()
+dotenv.config();
 
-mongoose.connect(process.env.MONGO_ATLAS_URL).then(() => {
-  console.log('Connected to DB');
-}).catch((err) =>{
-  console.log(err)
-})
+mongoose
+  .connect(process.env.MONGO_ATLAS_URL)
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.send("Hi");
 });
 
-app.use('/api/v1/users',userRoutes);
-app.use('/api/v1/auth', authRouter);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auth", authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Running on port: ${process.env.PORT}`);

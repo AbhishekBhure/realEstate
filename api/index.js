bc1f7a7208cc218abcd3074e8b52fe9f6d97959b
kiran <kiransagarwal126@gmail.com>
2023-12-02 13:34:02 +0530

    connected to MongoAtlas DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import dotenv from "dotenv"
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 const PORT = 3000;
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_ATLAS_URL).then(() => {
+  console.log('Connected to DB');
+}).catch((err) =>{
+  console.log(err)
+})
 
 app.get("/", (req, res) => {
   res.send("Hi");

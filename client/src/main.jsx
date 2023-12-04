67d8738e543c9f9664bf1d7f005587712c8cf8b0
abhi <abhiab7411@gmail.com>
2023-12-04 19:25:03 +0530

    adding redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
+import { persistor, store } from "./redux/store.js";
 import { Provider } from "react-redux";
 import { SnackbarProvider } from "notistack";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <SnackbarProvider>
-      <App />
-    </SnackbarProvider>
+    <PersistGate loading={null} persistor={persistor}>
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
+    </PersistGate>
   </Provider>
 );

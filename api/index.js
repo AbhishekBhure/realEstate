fd027fc38b30dcc68b26c6fd5bfd3b347d6dbe27
abhi <abhiab7411@gmail.com>
2023-12-06 19:54:23 +0530

    added route in index.js

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 dotenv.config();
 

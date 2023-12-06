f93686e9a13c96780cd74d03b5b20e81b0383b03
abhi <abhiab7411@gmail.com>
2023-12-06 19:53:34 +0530

    added logic to update user in userController

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,35 @@
-export const getUsers = (req,res) => {
-    res.send('hello user controller')
-}
+import User from "../models/userModel.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
+
+export const getUsers = (req, res) => {
+  res.send("hello user controller");
+};
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "Unauthorized"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};

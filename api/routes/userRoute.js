9d8d09ac9d9ac579f4ecacf187102ddb440fdfd4
abhi <abhiab7411@gmail.com>
2023-12-06 19:55:16 +0530

    installed cookie-parser and added userRoute

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,10 @@
-import express from "express"
-import { getUsers } from "../controllers/userController.js";
+import express from "express";
+import { getUsers, updateUser } from "../controllers/userController.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/", getUsers)
-
+router.get("/", getUsers);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;

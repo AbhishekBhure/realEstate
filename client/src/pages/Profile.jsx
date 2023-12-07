611eae2f347090298f2891de467a34b599ac9ab6
abhi <abhiab7411@gmail.com>
2023-12-07 23:14:04 +0530

    updated the user from frontend

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import UpdateProfile from "../components/UpdateProfile";
 import { useEffect, useRef, useState } from "react";
+import { useSnackbar } from "notistack";
+import Loader from "../components/Loader";
 import {
   getDownloadURL,
   getStorage,
@@ -8,14 +10,23 @@ import {
   uploadBytesResumable,
 } from "firebase/storage";
 import { app } from "../firebase";
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+} from "../redux/user/userSlice";
 
 const Profile = () => {
   const fileRef = useRef();
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, loading, error } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined);
   const [filePercentage, setFilePercentage] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
+  const [load, setLoad] = useState(false);
+
+  const dispatch = useDispatch();
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     if (file) {
@@ -46,11 +57,43 @@ const Profile = () => {
       }
     );
   };
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoad(true);
+      dispatch(updateUserStart);
+      const res = await fetch(`/api/v1/users/update/${currentUser._id}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        setLoad(false);
+        dispatch(updateUserFailure(data.message));
+        return;
+      }
+      setLoad(false);
+      dispatch(updateUserSuccess(data));
+      enqueueSnackbar("Profile Updated Successfully", { variant: "success" });
+    } catch (error) {
+      setLoad(false);
+      dispatch(updateUserFailure(error.message));
+      enqueueSnackbar(error.message, { variant: "error" });
+    }
+  };
   return (
     <div className="text-center max-w-lg mx-auto">
       <h1 className="futura-font text-3xl my-7 text-center">Profile</h1>
       <h3 className="mb-4 italic">Welcome {currentUser.username}</h3>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="file"
           ref={fileRef}
@@ -81,23 +124,31 @@ const Profile = () => {
         <input
           type="text"
           id="username"
+          defaultValue={currentUser.username}
           placeholder="username"
+          onChange={handleChange}
           className=" border p-3 focus:outline-none focus:rounded-2xl transition-all focus:border-slate-500 duration-700 rounded-lg"
         />
         <input
           type="email"
+          defaultValue={currentUser.email}
           id="email"
           placeholder="email"
+          onChange={handleChange}
           className=" border p-3 focus:outline-none focus:rounded-2xl transition-all focus:border-slate-500 duration-700 rounded-lg "
         />
         <input
           id="password"
           type="password"
           placeholder="password"
+          onChange={handleChange}
           className=" border p-3 focus:outline-none focus:rounded-2xl transition-all focus:border-slate-500 duration-700 rounded-lg "
         />
-        <button className="bg-slate-700 rounded-lg text-white uppercase hover:opacity-95 p-3 disabled:opacity-80 transition-all duration-500 ">
-          update
+        <button
+          disabled={loading}
+          className="bg-slate-700 rounded-lg text-white uppercase hover:opacity-95 p-3 disabled:opacity-80 transition-all duration-500 "
+        >
+          {load ? <Loader /> : "update"}
         </button>
       </form>
       <div className="flex justify-between mt-5">
@@ -108,6 +159,7 @@ const Profile = () => {
           sign out
         </button>
       </div>
+      <p className="text-red-700">{error ? error : ""}</p>
       <>
         <UpdateProfile />
       </>

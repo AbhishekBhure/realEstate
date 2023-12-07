bf0eb116db4a086275eb4bfea0bc1b1ecc16b70e
abhi <abhiab7411@gmail.com>
2023-12-07 23:20:37 +0530

    added delete route and delete controller in backend

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -33,3 +33,14 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+
+export const deleteUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "Unauthorized -> Delete your own account"));
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User Deleted Successfully!!");
+  } catch (error) {
+    next(error);
+  }
+};
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
-import { getUsers, updateUser } from "../controllers/userController.js";
+import {
+  getUsers,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/", getUsers);
 router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;

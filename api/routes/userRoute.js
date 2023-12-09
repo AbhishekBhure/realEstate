7d2087f1a2a38454d971abe125d4fc9940284041
abhi <abhiab7411@gmail.com>
2023-12-09 23:28:15 +0530

    created get all listings created by a particular user in backend

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -3,6 +3,7 @@ import {
   getUsers,
   updateUser,
   deleteUser,
+  getUserListings,
 } from "../controllers/userController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.get("/", getUsers);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;

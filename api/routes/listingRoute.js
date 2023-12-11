58784ddbd6c9b950409bf9310d66ee39a6619aac
abhi <abhiab7411@gmail.com>
2023-12-11 13:41:17 +0530

    adding listing route to update and get a single listing

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -2,6 +2,8 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  getListing,
+  updateListing,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -9,5 +11,7 @@ const router = express.Router();
 
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
+router.get("/singleListing/:id", getListing);
+router.post("/update/:id", verifyToken, updateListing);
 
 export default router;

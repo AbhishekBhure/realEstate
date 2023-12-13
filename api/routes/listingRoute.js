8d2a8b1584bbbfbd3ac7d7b355b9203e3eee8a20
abhi <abhiab7411@gmail.com>
2023-12-13 16:42:10 +0530

    added search api route and controller

--- api/routes/listingRoute.js
+++ api/routes/listingRoute.js
@@ -4,6 +4,7 @@ import {
   deleteListing,
   getListing,
   updateListing,
+  getListings,
 } from "../controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -13,5 +14,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.get("/singleListing/:id", getListing);
 router.post("/update/:id", verifyToken, updateListing);
+router.get("/getListings", getListings);
 
 export default router;

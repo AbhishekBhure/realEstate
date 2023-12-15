5519fd7090b4dd4c9f7d417947ddc7cf3d889127
abhi <abhiab7411@gmail.com>
2023-12-15 19:40:50 +0530

    deleted vercel and updated index.js file

--- api/index.js
+++ api/index.js
@@ -5,18 +5,11 @@ import userRoutes from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
 import listingRouter from "./routes/listingRoute.js";
 import cookieParser from "cookie-parser";
-// import path from "path";
+import path from "path";
 import cors from "cors";
 
 dotenv.config();
 const app = express();
-app.use(
-  cors({
-    origin: ["https://snap-space.vercel.app/api/v1"],
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true,
-  })
-);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -30,17 +23,17 @@ mongoose
     console.log(err);
   });
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/listing", listingRouter);
 
-// app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
